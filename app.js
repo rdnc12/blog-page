45f247bcca963a6e66918448f63acdff411c7afa
Erdinc <Erdinc@gklfmg.com>
2019-10-11 13:32:35 +0200

    established mongoose connection

--- app.js
+++ app.js
@@ -2,25 +2,41 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require('lodash');
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-var postArray = [];
-var nameList = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB",
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+const postsSchema = { header: String, content:String };
+const Post = mongoose.model("Title", postsSchema);
+const Post1 = new Post({ header: "Welcome", content: "First post has been send!!!" });
+const defaultPost=[Post1];
+
+var postArray = [];
+var nameList = [];
+
 
 app.get('/', (req, res) => {
-  res.render('home', { home: homeStartingContent, postNew: postArray, menuId: "home" });
+
+  Post.find({}, (err, foundPost) => {
+    if (foundPost.length === 0) {
+      Post.insertMany(defaultPost, (err) => {
+        if (err) { console.log(err); }
+        else { console.log("success!!!"); }
+      });
+      res.redirect("/");
+    }
+    else
+      res.render('home', { home: defaultPost[0].content, postNew: postArray, menuId: "home" });
+  });
+
 });
 
 
@@ -35,15 +51,15 @@ app.get('/contact', (req, res) => {
 
 
 app.get('/compose', (req, res) => {
-  if (nameList.length !== 0) 
-    res.render('compose',{ menuId: "compose" });
+  if (nameList.length !== 0)
+    res.render('compose', { menuId: "compose" });
   else
     res.status(401).send('<h1>401 Unauthorized</h1><p>Please first login!!!</p>');
 });
 
 
 app.get('/login', (req, res) => {
-  res.render('login', {menuId: "login"});
+  res.render('login', { menuId: "login" });
 });
 
 
@@ -65,10 +81,10 @@ app.post('/compose', (req, res) => {
     title: req.body.postTitle,
     posting: req.body.postArea
   };
-    postArray.push(publishPost);
+  postArray.push(publishPost);
   nameList = [];
-    res.redirect('/');
-    
+  res.redirect('/');
+
 });
 
 
@@ -80,12 +96,10 @@ app.get('/posts/:title', (req, res) => {
     var storedTitle = value.title;
 
     if (_.lowerCase(titlePost) === _.lowerCase(storedTitle))
-      res.render('post', { newRender: value, menuId: 'newpost'  });
+      res.render('post', { newRender: value, menuId: 'newpost' });
   });
 
 });
-
-
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
